936f2842b408171049ac867ec160e93804b210b1
Seb-SOULIER <soulier.sebastien.ss@gmail.com>
2021-04-08 09:24:09 +0200

    Quete Composer

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__.'/../src/wcs/Hello.php';
+
+$text=new Hello();
+echo $text->talk();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c1a5bc87dd2a885e1c41636b9ea0e2ba9fa8f865',
+    'name' => 'soulier/twig1',
+  ),
+  'versions' => 
+  array (
+    'soulier/twig1' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c1a5bc87dd2a885e1c41636b9ea0e2ba9fa8f865',
+    ),
+  ),
+);
